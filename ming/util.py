7457733fe7622957e117c299cd98263d0af1d173
mingming741 <419955347@qq.com>
2020-03-27 16:30:12 +0800

    start evaulation

--- ming/util.py
+++ ming/util.py
@@ -1,6 +1,7 @@
-from collections import defaultdict
+import time
+import inspect
 import copy
-
+from collections import deque
 
 def merge_config(new_config, old_config):
     """Merge the user-defined config with default config"""
@@ -8,3 +9,6 @@ def merge_config(new_config, old_config):
     if new_config is not None:
         config.update(new_config)
     return config
+
+def wait(sleep=0.2):
+    time.sleep(sleep)
